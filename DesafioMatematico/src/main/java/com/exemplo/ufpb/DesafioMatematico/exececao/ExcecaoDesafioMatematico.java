e911bfa12b4ab6ea292a463c55cd85b4d5846287
rodrigovilar <rvilar@gmail.com>
2014-08-19 08:09:30 -0300

    Correcao 1

--- DesafioMatematico/src/main/java/com/exemplo/ufpb/DesafioMatematico/exececao/ExcecaoDesafioMatematico.java
+++
@@ -1,8 +0,0 @@
-package com.exemplo.ufpb.DesafioMatematico.exececao;
-
-public class ExcecaoDesafioMatematico extends RuntimeException{
-
-	public ExcecaoDesafioMatematico(String msg){
-		super(msg);
-	}
-}
